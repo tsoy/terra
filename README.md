f2afe007873e6c4fe93f78f59a5c1aefd83cec68
Sergey Tsoy <tsoy.sergey@hotmail.com>
2021-04-10 23:42:36 -0400

    Install nginx in user-data; add security group

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-aws credentials are stored in ~/.aws/credentials
+aws credentials are stored in ~/.aws/credentials 
 on mac terraform may be moved to /usr/local/bin/
+Within each region, there are multiple isolated datacenters
+known as Availability Zones (AZs)
 
-Within each region, there are multiple isolated datacenters known as Availability Zones (AZs), such as
-, , and so on
+user_data:
+```
+sudo amazon-linux-extras install nginx1 -y
+echo 'Hello' > /usr/share/nginx/html/index.html 
+sudo systemctl start nginx
+```
+You need specify outbound rules for a security group in order to install something 
+
+
+
--- main.tf
+++ main.tf
@@ -5,8 +5,41 @@ provider "aws" {
 resource "aws_instance" "my-book-instance" {
   ami = "ami-05d72852800cbf29e"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.for-book-instance.id]
+
+  user_data = <<EOF
+          #!/bin/bash
+          amazon-linux-extras install nginx1 -y
+          echo 'Hello' > /usr/share/nginx/html/index.html
+          systemctl start nginx
+          echo 'Nice'
+          EOF
 
   tags = {
     Name = "terraform-book-example"
+  }
+}
+
+resource "aws_security_group" "for-book-instance" {
+  name = "HTTP and SSH"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port = 0
+    protocol = "-1"
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
