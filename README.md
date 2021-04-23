ccddbbe474e56c6a7f99d2bed375e22efb620562
Sergey Tsoy <tsoy.sergey@hotmail.com>
2021-04-23 19:48:25 -0400

    Add input variable for nginx port

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-aws credentials are stored in ~/.aws/credentials 
+aws credentials are stored in ~/.aws/credentials. Terraform will use them
 on mac terraform may be moved to /usr/local/bin/
 Within each region, there are multiple isolated datacenters
 known as Availability Zones (AZs)
@@ -9,7 +9,8 @@ sudo amazon-linux-extras install nginx1 -y
 echo 'Hello' > /usr/share/nginx/html/index.html 
 sudo systemctl start nginx
 ```
-You need specify outbound rules for a security group in order to install something 
+You need specify outbound rules for a security group in order to install something
 
+To allow you to make your code more DRY and more configurable, Terraform allows you to define input variables.
 
 
--- main.tf
+++ main.tf
@@ -1,16 +1,23 @@
 provider "aws" {
   region = "us-east-2"
 }
+variable "web_server_port" {
+  description = "The port web server will use for HTTP requests"
+  type = number
+}
 
 resource "aws_instance" "my-book-instance" {
   ami = "ami-05d72852800cbf29e"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.for-book-instance.id]
+  vpc_security_group_ids = [
+    aws_security_group.for-book-instance.id
+  ]
 
   user_data = <<EOF
           #!/bin/bash
           amazon-linux-extras install nginx1 -y
           echo 'Hello' > /usr/share/nginx/html/index.html
+          sed -i 's/80/${var.web_server_port}/' /etc/nginx/nginx.conf
           systemctl start nginx
           echo 'Nice'
           EOF
@@ -23,8 +30,8 @@ resource "aws_instance" "my-book-instance" {
 resource "aws_security_group" "for-book-instance" {
   name = "HTTP and SSH"
   ingress {
-    from_port = 80
-    to_port = 80
+    from_port = var.web_server_port
+    to_port = var.web_server_port
     protocol = "tcp"
     cidr_blocks = [
       "0.0.0.0/0"
@@ -34,12 +41,16 @@ resource "aws_security_group" "for-book-instance" {
     from_port = 22
     protocol = "tcp"
     to_port = 22
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
   }
   egress {
     from_port = 0
     protocol = "-1"
     to_port = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
   }
 }
